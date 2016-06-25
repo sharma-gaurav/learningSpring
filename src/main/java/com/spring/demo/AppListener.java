ff5f1844221655a1503f3bef533ac2e4a8e22f37
gaurav-sharma <gaurav.sharma@tothenew.com>
2016-06-25 12:48:20 +0530

    Renamed file to app listener

--- src/main/java/com/spring/demo/AppListener.java
+++ src/main/java/com/spring/demo/AppListener.java
@@ -3,12 +3,20 @@ package com.spring.demo;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.ContextStartedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AppListener implements ApplicationListener<ContextRefreshedEvent> {
-    @Override
-    public void onApplicationEvent(ContextRefreshedEvent event) {
+public class AppListener {
+
+    @EventListener(ContextRefreshedEvent.class)
+    public void onEvent(ContextRefreshedEvent event) {
+        System.out.println(event);
+    }
+
+    @EventListener(ContextStartedEvent.class)
+    public void onEvent(ContextStartedEvent event) {
         System.out.println(event);
     }
 }
